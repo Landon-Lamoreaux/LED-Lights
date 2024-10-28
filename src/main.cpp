6a43e576024bf505a992c4d06ca82d4255bdc199
Landon Lamoreaux <Ljlamoreaux15@gmail.com>
2024-10-28 12:44:23 -0700

    Added a halloween and a christmas mode.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 
 #include <FastLED.h>
-#define NUM_LEDS 225
+#define NUM_LEDS 100
 #define DATA_PIN 13
 //#define BRIGHTNESS 200   /* Control the brightness of your leds */
 #define SATURATION 255   /* Control the saturation of your leds */
@@ -13,9 +13,14 @@ CRGB leds[NUM_LEDS];
 uint8_t r, g, b;
 int potValue = 0;
 
-bool rainbow = true;
+bool rainbow = false;
+bool colors = false;
+bool halloween = true;
+bool christmas = false;
 int buttonState;
 int lastButtonState;
+
+unsigned long lastmillis = 0;
 
 void setup() 
    { 
@@ -23,7 +28,7 @@ void setup()
       pinMode(BUTTON_PIN, INPUT);
       pinMode(POT_PIN, INPUT);
       FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);    
-      r = g = b = 0;  
+      r = g = b = 0;
    }
 
 void loop()
@@ -32,10 +37,11 @@ void loop()
   {
     for (int j = 0; j < 255; j++) {
       for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the value 4 the less fade there is and vice versa */ 
+        leds[i] = CHSV(i - (j * 2), SATURATION, 255); /* The higher the value 4 the less fade there is and vice versa */ 
       }
       delay(50); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
       BRIGHTNESS = (analogRead(POT_PIN) / 5) % 255;
+      FastLED.setBrightness(BRIGHTNESS);
       FastLED.show();
 
       // Checking if the button was pressed to change out of rainbow mode.
@@ -44,11 +50,12 @@ void loop()
       if(buttonState == HIGH && lastButtonState == LOW)
       {
         rainbow = false;
+        colors = true;
         break;
       }
     }
   }
-  else
+  else if (colors)
   {
     // Adjusting the LED color by the value of the potentiometer.
     BRIGHTNESS = 200;
@@ -57,6 +64,105 @@ void loop()
       leds[i] = CHSV(potValue, SATURATION, BRIGHTNESS);
     }
     potValue = (analogRead(POT_PIN) / 4) % 255;  // Reading the poteniometer value.
+    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.show();
+  }
+  else if(halloween)
+  {
+    if(millis() - lastmillis <= 500)
+    {
+      for(int i = 0; i < NUM_LEDS; i++)
+      {
+        if(i % 3 == 0)
+          leds[i] = CRGB(0xFF7A00);
+        else if(i % 3 == 1)
+          leds[i] = CRGB(0x6BFF00);
+        else
+          leds[i] = CRGB(0x0061FF);
+      }
+    }
+    if(millis() - lastmillis > 500 && millis() - lastmillis < 1000)
+    {
+      for(int i = 0; i < NUM_LEDS; i++)
+      {
+        if(i % 3 == 0)
+          leds[i] = CRGB(0x0061FF);
+        else if(i % 3 == 1)
+          leds[i] = CRGB(0xFF7A00);
+        else
+          leds[i] = CRGB(0x6BFF00);
+      }
+    }
+    if(millis() - lastmillis >= 1000)
+    {
+      for(int i = 0; i < NUM_LEDS; i++)
+      {
+        if(i % 3 == 0)
+          leds[i] = CRGB(0x6BFF00);
+        else if(i % 3 == 1)
+          leds[i] = CRGB(0x0061FF);
+        else
+          leds[i] = CRGB(0xFF7A00);
+      }
+    }
+    if(millis() - lastmillis >= 1500)
+      lastmillis = millis();
+
+    BRIGHTNESS = (analogRead(POT_PIN) / 5) % 255;
+    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.show();
+  }
+  else if(christmas)
+  {
+    for(int i = 0; i < NUM_LEDS - 2; i = i + 3)
+    {
+      leds[i] = CRGB(0x00B30C);
+      // leds[i + 1] = CRGB(0xB3002C);
+      leds[i + 1] = CRGB(0x590D01);
+      leds[i + 2] = CRGB(0xFFFFFF);
+    }
+
+    if(millis() - lastmillis <= 500)
+    {
+      for(int i = 0; i < NUM_LEDS; i++)
+      {
+        if(i % 3 == 0)
+          leds[i] = CRGB(0x00B30C);
+        else if(i % 3 == 1)
+          leds[i] = CRGB(0x590D01);
+        else
+          leds[i] = CRGB(0xFFFFFF);
+      }
+    }
+    if(millis() - lastmillis > 500 && millis() - lastmillis < 1000)
+    {
+      for(int i = 0; i < NUM_LEDS; i++)
+      {
+        if(i % 3 == 0)
+          leds[i] = CRGB(0xFFFFFF);
+        else if(i % 3 == 1)
+          leds[i] = CRGB(0x00B30C);
+        else
+          leds[i] = CRGB(0x590D01);
+      }
+    }
+    if(millis() - lastmillis >= 1000)
+    {
+      for(int i = 0; i < NUM_LEDS; i++)
+      {
+        if(i % 3 == 0)
+          leds[i] = CRGB(0x590D01);
+        else if(i % 3 == 1)
+          leds[i] = CRGB(0xFFFFFF);
+        else
+          leds[i] = CRGB(0x00B30C);
+      }
+    }
+    if(millis() - lastmillis >= 1500)
+      lastmillis = millis();
+
+    BRIGHTNESS = (analogRead(POT_PIN) / 5) % 255;
+    FastLED.setBrightness(BRIGHTNESS);
     FastLED.show();
   }
 
@@ -67,7 +173,20 @@ void loop()
   {
     if(rainbow == true)
       rainbow = false;
-    else
+    else if(colors)
+    {
+      halloween = true;
+      colors = false;
+    }
+    else if(halloween)
+    {
+      christmas = true;
+      halloween = false;
+    }
+    else if(christmas)
+    {
       rainbow = true;
+      christmas = false;
+    }
   }
 }
